e29b8af8c04b329feda90bce1ff7a4ffdab6f0b8
lxndr <lxndr.nonstop@gmail.com>
2024-02-05 01:11:25 +0300

    Refactored ItemController
    - Moved mapping from api/v1/warehouse to api/v1/item
    - Removed "Item" from each method name. I.E getItem -> get
    - create and edit method were merged into one save method
    - created method to work to create new Item or change an old one from DTO

--- src/main/java/ru/yourwarehouse/warehouseapi/controller/ItemController.java
+++ src/main/java/ru/yourwarehouse/warehouseapi/controller/ItemController.java
@@ -1,42 +1,69 @@
 package ru.yourwarehouse.warehouseapi.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.*;
+import ru.yourwarehouse.warehouseapi.dto.ItemDTO;
+import ru.yourwarehouse.warehouseapi.entity.Category;
 import ru.yourwarehouse.warehouseapi.entity.Item;
+import ru.yourwarehouse.warehouseapi.service.CategoryService;
 import ru.yourwarehouse.warehouseapi.service.ItemService;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping("/api/v1/warehouse")
+@RequestMapping("/api/v1/item")
 @AllArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
 
+    private final CategoryService categoryService;
+
     @GetMapping("/{id}")
-    public Optional<Item> getItem(@PathVariable UUID id) {
+    public Optional<Item> get(@PathVariable UUID id) {
         return itemService.get(id);
     }
 
     @PostMapping
-    public Optional<Item> createItem(@RequestBody Item item) {
-        return itemService.save(item);
-    }
-
-    @PostMapping
-    public Optional<Item> editItem(@RequestBody Item item) {
+    public Optional<Item> save(@RequestBody ItemDTO dto) {
+        Item item = createItemFromDTO(dto);
         return itemService.save(item);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteItem(@PathVariable UUID id) {
+    public void delete(@PathVariable UUID id) {
         itemService.delete(id);
     }
 
     @DeleteMapping
-    public void deleteItem(@RequestBody Item item) {
+    public void delete(@RequestBody ItemDTO dto) {
+        Item item = createItemFromDTO(dto);
         itemService.delete(item);
     }
+
+    @SneakyThrows
+    private Item createItemFromDTO(ItemDTO dto) {
+        CompletableFuture<Category> categoryFuture = CompletableFuture
+                .supplyAsync(() -> categoryService.get(dto.getCategory()).get());
+
+        Item item;
+
+        if (dto.getArticle() != null) {
+            item = itemService.get(dto.getArticle()).get();
+        } else {
+            item = new Item();
+        }
+
+        item.setArticle(dto.getArticle());
+        item.setName(dto.getName());
+        item.setDescription(dto.getDescription());
+        item.setCategory(categoryFuture.get());
+        item.setCount(dto.getCount());
+
+        return item;
+    }
+
 }
